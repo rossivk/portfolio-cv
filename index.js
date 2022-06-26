76f4c825112eb20bb28b49cb411775dd4d6fb736
Rosica Kirkova <96499964+rossivk@users.noreply.github.com>
2022-06-26 16:22:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,26 +8,17 @@ window.onbeforeunload = function () {
 
 // tl.from(".github, .twitter, .linkedin", { duration: 1, delay: -0.5, opacity: 0, x: 200, stagger: 0.25, ease: "bounce" });
 
-const tl = new TimelineMax();
+
 const socialIcons = document.querySelectorAll(".github, .twitter, .linkedin, .hamburger-lines");
 
-let delayAnimation = 1;
+let delayAnimation = 0.5;
 
-tl.from(".tittle", 3, {
-  scale: 0.1,
-  ease: Back.easeOut
-});
-
-tl.from(".nav-container .logo", 3, {
-  delay: delayAnimation - 0.9,
-  x: -300,
-  ease: Bounce.easeOut
-});
-
-tl.staggerFrom(socialIcons, 2, {
-  y: -400,
-  ease: Bounce.easeOut
-}, 0.05);
+gsap.timeline()
+  .from("#top", { duration: 0.5, opacity: 0, ease: "back" })
+  .from(".tittle", { opacity: 0, scale: 0, ease: "back" })
+  .from(".nav-container .logo", { duration: 2, opacity: 0, x: -300, ease: Back.easeOut })
+  .from(socialIcons, { duration: 3, stagger: 0.05, y: -400, ease: Bounce.easeOut })
+  .from(".top-section-down", { opacity: 0, ease: "back" })
 
 // tl.from(".hamburger-lines", 2, {
 //   delay: delayAnimation - 0.9,

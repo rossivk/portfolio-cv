aae60b6a4aebc48da28ff54744f80837347ce00f
Rosica Kirkova <96499964+rossivk@users.noreply.github.com>
2022-06-26 12:42:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,16 +8,29 @@ window.onbeforeunload = function () {
 
 // tl.from(".github, .twitter, .linkedin", { duration: 1, delay: -0.5, opacity: 0, x: 200, stagger: 0.25, ease: "bounce" });
 
-const tl = new TimelineMax(),
-  bouncy = document.querySelectorAll(".github, .twitter, .linkedin")
+const tl = new TimelineMax();
+const socialIcons = document.querySelectorAll(".github, .twitter, .linkedin, .hamburger-lines");
 
-tl.staggerFrom(bouncy, 3, {
+let delayAnimation = 1;
+
+tl.from(".tittle", 3, {
+  scale: 0.1,
+  ease: Back.easeOut
+});
+
+tl.from(".nav-container .logo", 3, {
+  delay: delayAnimation - 0.9,
+  x: -300,
+  ease: Bounce.easeOut
+});
+
+tl.staggerFrom(socialIcons, 2, {
   y: -400,
   ease: Bounce.easeOut
 }, 0.05);
 
-tl.from(".hamburger-lines", 3, {
-  delay: +1.5,
-  x: 250,
-  ease: Bounce.easeOut
-}, 0.05);
+// tl.from(".hamburger-lines", 2, {
+//   delay: delayAnimation - 0.9,
+//   x: 250,
+//   ease: Bounce.easeOut
+// });
